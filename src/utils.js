03904aa43f97d4a7d9300b5b48d7997c725ae998
Casey O'Hara <caseyo@caseyohara.com>
2011-05-21 16:11:39 -0600

    Fixed a sloppy ternary expression

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,13 @@
 var Utils = Utils || {
 	
 	extend : function(params, defaults) {
-		if ( typeof params === "object" ) {
-			for ( var index in defaults ) {
-				if ( typeof params[index] === "undefined" ) { 
-					params[index] = defaults[index]; 
-				}
-			}
-			return params;
-		}
-		else {
-			return defaults;
-		}
-	}
+		if ( typeof params !== "object" ) { return defaults };
 
+		for ( var index in defaults ) {
+			if ( typeof params[index] === "undefined" ) { params[index] = defaults[index]; };
+		}
+		
+		return params;
+	}
 	
 };
